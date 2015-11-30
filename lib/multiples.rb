045359d70e0fec19d49a35cbd0c7a1bade064e00
mindyyuan <mindyuan@gmail.com>
2015-11-29 23:07:51 -0500

    Update multiples.rb

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def natural(number)
+  i = 1
+  sum = 0
+  while i < number
+    if (i % 3 == 0) || (i % 5 == 0)
+      sum += i
+    end
+  i += 1
+  end
+end
+
+natural(1000)
