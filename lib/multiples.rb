d2b961400b4451db2535859f8f4a86512e3fef4a
mindyyuan <mindyuan@gmail.com>
2016-04-17 19:37:54 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  arr = []
+  n = 1
+  while n < limit
+    if n % 3 == 0 || n % 5 == 0
+      arr.push(n)
+    end
+    n+=1
+  end
+  arr
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    arr = []
+    n = 1
+    while n < limit
+      if n % 3 == 0 || n % 5 == 0
+        arr.push(n)
+      end
+      n+=1
+    end
+    arr
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+
+end
